13bdeecf74dd929f5888957a24960411e53077fc
Ir0nBlade <itsvimal@yahoo.com>
2021-12-27 13:31:28 +0000

    Second Commit 27/12

--- target/cucumber-report/cucumber.html/report.js
+++ target/cucumber-report/cucumber.html/report.js
@@ -1,233 +1,80 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/java/resources/featurefile/ShoppingCartDelete.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/java/resources/featurefile/OurStore.feature");
 formatter.feature({
   "line": 1,
-  "name": "Shopping cart delete",
-  "description": "\r\nUser should able to delete product from shopping cart",
-  "id": "shopping-cart-delete",
+  "name": "Our Stores",
+  "description": "\r\nUser should browse our strore page successfully",
+  "id": "our-stores",
   "keyword": "Feature"
 });
-formatter.scenarioOutline({
-  "line": 7,
-  "name": "Verify user should remove product from cart successfully",
-  "description": "",
-  "id": "shopping-cart-delete;verify-user-should-remove-product-from-cart-successfully",
-  "type": "scenario_outline",
-  "keyword": "Scenario Outline"
-});
-formatter.step({
-  "line": 8,
-  "name": "I click on \u0027Women\u0027 tab",
-  "keyword": "When "
-});
-formatter.step({
-  "line": 9,
-  "name": "I select product \"\u003cPRODUCT\u003e\"",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 10,
-  "name": "I select quantity \"\u003cQUANTITY\u003e\", size \"\u003cSIZE\u003e\" and colour \"\u003cCOLOUR\u003e\"",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 11,
-  "name": "I click on \u0027Add To Cart\u0027 button",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 12,
-  "name": "I can see product has been \u0027Product successfully added to your shopping cart\u0027",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 13,
-  "name": "I click on \u0027Proceed To Checkout\u0027 button",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 14,
-  "name": "I click on \u0027Delete\u0027 icon to delete the product from shopping cart",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 15,
-  "name": "Product has been removed from shopping cart successfully",
-  "keyword": "Then "
-});
-formatter.examples({
-  "line": 16,
-  "name": "",
-  "description": "",
-  "id": "shopping-cart-delete;verify-user-should-remove-product-from-cart-successfully;",
-  "rows": [
-    {
-      "cells": [
-        "PRODUCT",
-        "QUANTITY",
-        "SIZE",
-        "COLOUR"
-      ],
-      "line": 17,
-      "id": "shopping-cart-delete;verify-user-should-remove-product-from-cart-successfully;;1"
-    },
-    {
-      "cells": [
-        "Blouse",
-        "2",
-        "M",
-        "White"
-      ],
-      "line": 18,
-      "id": "shopping-cart-delete;verify-user-should-remove-product-from-cart-successfully;;2"
-    }
-  ],
-  "keyword": "Examples"
-});
 formatter.before({
-  "duration": 7410681200,
+  "duration": 11887464900,
   "status": "passed"
 });
 formatter.background({
   "line": 5,
-  "name": "I am on homepage",
+  "name": "I am on Homepage",
   "description": "",
   "type": "background",
   "keyword": "Background"
 });
 formatter.scenario({
-  "line": 18,
-  "name": "Verify user should remove product from cart successfully",
+  "line": 7,
+  "name": "Verify User should able to browse Store Page successfully",
   "description": "",
-  "id": "shopping-cart-delete;verify-user-should-remove-product-from-cart-successfully;;2",
+  "id": "our-stores;verify-user-should-able-to-browse-store-page-successfully",
   "type": "scenario",
-  "keyword": "Scenario Outline"
+  "keyword": "Scenario"
 });
 formatter.step({
   "line": 8,
-  "name": "I click on \u0027Women\u0027 tab",
+  "name": "I click on \u0027Our Stores\u0027 link",
   "keyword": "When "
 });
 formatter.step({
   "line": 9,
-  "name": "I select product \"Blouse\"",
-  "matchedColumns": [
-    0
-  ],
+  "name": "I accept Pop Up message",
   "keyword": "And "
 });
 formatter.step({
   "line": 10,
-  "name": "I select quantity \"2\", size \"M\" and colour \"White\"",
-  "matchedColumns": [
-    1,
-    2,
-    3
-  ],
+  "name": "I scroll Through Map to West Palm Beach",
   "keyword": "And "
 });
 formatter.step({
   "line": 11,
-  "name": "I click on \u0027Add To Cart\u0027 button",
+  "name": "I take Screenshot of the page",
   "keyword": "And "
 });
-formatter.step({
-  "line": 12,
-  "name": "I can see product has been \u0027Product successfully added to your shopping cart\u0027",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 13,
-  "name": "I click on \u0027Proceed To Checkout\u0027 button",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 14,
-  "name": "I click on \u0027Delete\u0027 icon to delete the product from shopping cart",
-  "keyword": "And "
-});
-formatter.step({
-  "line": 15,
-  "name": "Product has been removed from shopping cart successfully",
-  "keyword": "Then "
-});
 formatter.match({
-  "location": "ShoppingCartDelete.iClickOnWomenTab()"
+  "location": "OurStores.iClickOnOurStoresLink()"
 });
 formatter.result({
-  "duration": 2414356400,
+  "duration": 7019493600,
   "status": "passed"
 });
 formatter.match({
-  "arguments": [
-    {
-      "val": "Blouse",
-      "offset": 18
-    }
-  ],
-  "location": "ShoppingCartDelete.iSelectProduct(String)"
+  "location": "OurStores.iAcceptPopUpMessage()"
 });
 formatter.result({
-  "duration": 1679353500,
+  "duration": 41418099,
   "status": "passed"
 });
 formatter.match({
-  "arguments": [
-    {
-      "val": "2",
-      "offset": 19
-    },
-    {
-      "val": "M",
-      "offset": 29
-    },
-    {
-      "val": "White",
-      "offset": 44
-    }
-  ],
-  "location": "ShoppingCartDelete.iSelectQuantitySizeAndColour(String,String,String)"
+  "location": "OurStores.iScrollThroughMapToWestPalmBeach()"
 });
 formatter.result({
-  "duration": 172579600,
+  "duration": 1955709200,
   "status": "passed"
 });
 formatter.match({
-  "location": "ShoppingCartDelete.iClickOnAddToCartButton()"
+  "location": "OurStores.iTakeScreenshotOfThePage()"
 });
 formatter.result({
-  "duration": 54725700,
-  "status": "passed"
-});
-formatter.match({
-  "location": "ShoppingCartDelete.iCanSeeProductHasBeenProductSuccessfullyAddedToYourShoppingCart()"
-});
-formatter.result({
-  "duration": 24773800,
-  "status": "passed"
-});
-formatter.match({
-  "location": "ShoppingCartDelete.iClickOnProceedToCheckoutButton()"
-});
-formatter.result({
-  "duration": 1937184700,
-  "status": "passed"
-});
-formatter.match({
-  "location": "ShoppingCartDelete.iClickOnDeleteIconToDeleteTheProductFromShoppingCart()"
-});
-formatter.result({
-  "duration": 43079400,
-  "status": "passed"
-});
-formatter.match({
-  "location": "ShoppingCartDelete.productHasBeenRemovedFromShoppingCartSuccessfully()"
-});
-formatter.result({
-  "duration": 23206500,
+  "duration": 1350748600,
   "status": "passed"
 });
 formatter.after({
-  "duration": 69200,
+  "duration": 603765201,
   "status": "passed"
 });
 });
